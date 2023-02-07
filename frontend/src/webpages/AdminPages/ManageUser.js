511073a14c40913d241bb95e02ac767e2b7595c0
Aditya Lamichhane <adityalamichhane3@gmail.com>
2023-02-07 17:25:01 +0545

    Implemented delete user functionality for admin

--- frontend/src/webpages/AdminPages/ManageUser.js
+++ frontend/src/webpages/AdminPages/ManageUser.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
 import AdminNav from '../Navbar/AdminNav'
 
 function ManageUser() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchUser();
+  },[])
+  const fetchUser= () =>{
+    fetch("http://localhost:8000/getAUser",{
+      method: "GET",
+    })
+    .then((res)=>res.json())
+    .then((data)=> {
+      console.log(data, "userData");
+      setData(data.data);
+    })
+  }
+  const removeUser = (id) => {
+    if(window.confirm(`Do you want to delete the user`)){
+      fetch("http://localhost:8000/removeUser",{
+        method:"POST",
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          userId : id
+        }),
+      })
+      .then((res)=>res.json())
+      .then((data)=>{
+        window.location.reload();
+        toast.success(data.data);
+        
+      })
+    }
+  }
   return (
     <>
     <AdminNav />
@@ -10,14 +47,22 @@ function ManageUser() {
       <h1>manageUser</h1>
       </div>
       <table>
-        <thead>
-          <th>UserName</th>
-          <th>Email</th>
-          <th>Action</th>
-        </thead>
-        <tbody>
-          
-        </tbody>
+          <tr>
+            <th>User Id</th>
+            <th>UserName</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        {data.map(i=>{
+            return(
+              <tr>
+                <td>{i._id}</td>
+                <td>{i.userName}</td>
+                <td>{i.email}</td>
+                <td><button onClick={()=>removeUser(i._id)}>Remove</button></td>
+                </tr>
+            )
+          })}
       </table>
       </>
   )
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const router = require("./route/userRoute");
 const adminRouter = require("./route/adminRoute");
 const cookie = require("cookie-parser");
 const cors = require("cors");
+
 const app = express();
 
 app.use(cors({credentials:true,origin:"http://localhost:3000"}));
@@ -19,3 +20,28 @@ mongoose.connect("mongodb+srv://admin:admin@cluster0.bq5eppq.mongodb.net/AuthDat
 .catch(
   (e)=> console.log(e)
 );
+const User = mongoose.model("user");
+app.get("/getAUser", async(req, res)=>{
+  try{
+    const allAUser = await User.find({});
+    res.send({ status: "ok", data: allAUser})
+  }
+  catch(e){
+    console.log(e);
+  }
+})
+app.post("/removeUser", async(req, res)=>{
+  const {userId} = req.body;
+
+  try{
+    User.deleteOne({_id:userId}, function(err, res){
+        console.log(err);
+      }
+    )
+    res.send({ status:"ok", data: "User Removed" });
+
+  }
+  catch(e){
+    console.log(e);
+  }
+})
